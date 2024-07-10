72e26663410322a80ace939c48797ddada7f64f1
ACLeightonWE <alex.cheung@leighton.com>
2024-07-10 15:07:32 +0100

    README Files updated

---
+++ backend/README.md
@@ -0,0 +1,17 @@
+# Task Manager - Backend Server
+
+A tool for organising and assigning of tasks
+
+Manages the database which stores the created tasks
+
+## Set-up Instructions
+
+From the backend folder run the command:
+
+### `npm start`
+
+This will start the backend server at `localhost:3001`
+
+### Both the frontend webserver and backend server should be started before use
+
+## Usage Instructions
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,57 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>Task Management</h1>
-      <p>HEllo world</p>
+  <html>
+    <div class="container">
+    <div class="FormStyle">
+    <form>
+        <div class="LeftFormStyle">
+            <h2>Add new Task</h2>
+            <p>Title:</p>
+            <input type="text" id="titleEntry" />
+            <p>Description:</p>
+            <textarea id="textEntry" cols="50" rows="5"></textarea>
+        </div>
+    </form>
+    <div class="UpperRightFormStyle">
+        <p>Heli</p>
+        <button onclick="addTask()">Hello</button>
     </div>
+    </div>
+
+
+  </div>
+  
+
+  
+  <h1 class="HeadTitle">TASK MANAGER</h1>
+
+  <div class="grid-container">
+    <div class="c1">
+        Available
+    </div>
+    <div class="c2">
+        In-Progress
+    </div>
+    <div class="c3">
+        Completed
+    </div>
+</div>
+
+<div class="grid-container">
+    <div class="item1" id="Available">
+        <div class="task">
+            <h1>Hiya</h1>
+            <body>
+                <p>FGo to scool</p>
+                <p>Due date: 10.5.20</p>
+            </body>
+        </div>
+    </div>
+    <div class="item2" id="In-Progress">2</div>
+    <div class="item3">3</div>
+  </div>
+  </html>
   );
 };
 
